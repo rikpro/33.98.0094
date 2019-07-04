f35d6c0f32e456980c5c9981b96dbc271a195829
Jenkins <jenkins@televic.com>
2019-07-04 11:15:31 -0700

    New generated testspecs

--- create_dummy_specfiles.py
+++ create_dummy_specfiles.py
@@ -4,4 +4,4 @@ foldername = "C:\\Program Files (x86)\\Jenkins\\workspace\\TestSpecifications\\3
 for i in range(1,3):
     filename = foldername + "testspec" + str(i) + ".txt"
     my_file = open(filename,"w+")
-    #my_file.write("Hey This text is going to be added to the text file yipeee!!!")
+    my_file.write("Hey This text is going to be added to the text file yipeee!!!")
